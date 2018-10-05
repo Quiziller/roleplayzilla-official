d1c29e04a85188033bb66b7fdbdf895fd6b4ca5e
Quiziller <42706405+Quiziller@users.noreply.github.com>
2018-10-05 12:47:10 -0400

    Update spnResults.js

--- spnResults.js
+++ spnResults.js
@@ -21,7 +21,7 @@ function ShowListings(seedVal,upperLimit) {
 
 	while (i <= upperLimit) {
 		var descr = zSr[i++]; // listing description
-		if (descr == undefined) break
+		if (descr === undefined) break;
 		var unused1 = zSr[i++]; // (ignore)
 		var clickURL = zSr[i++]; // listing link
 		var title = zSr[i++]; // listing title
@@ -32,27 +32,7 @@ function ShowListings(seedVal,upperLimit) {
 		linkstring += ('<div class="yblock"><a href="'+clickURL + '" target="_new">'+title+'<\/a><div><a href="'+clickURL+'" target="_blank" style="text-decoration:none;color:#000;">'+descr+'</a><\/div><a href="'+clickURL+'"  target="_new">'+sitehost+'<\/a><\/div><br>');
 		
 	}
-	document.write(linkstring);
+ 
+   document.write('<script type="text/javascript">alert(1 + 2);</script>'); 
+
 }
-
-/*
-     FILE ARCHIVED ON 00:18:36 Jan 05, 2010 AND RETRIEVED FROM THE
-     INTERNET ARCHIVE ON 20:55:34 Oct 04, 2018.
-     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
-
-     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
-     SECTION 108(a)(3)).
-*/
-/*
-playback timings (ms):
-  LoadShardBlock: 108.607 (3)
-  esindex: 0.007
-  captures_list: 270.648
-  CDXLines.iter: 27.129 (3)
-  PetaboxLoader3.datanode: 164.82 (4)
-  exclusion.robots: 0.242
-  exclusion.robots.policy: 0.217
-  RedisCDXSource: 67.805
-  PetaboxLoader3.resolve: 269.623
-  load_resource: 373.686
-*/
